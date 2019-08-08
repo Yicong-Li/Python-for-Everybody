8d86c495a17c5fc72b528a528fe0a0f9b05fd152
Jackson Li <jackson_li_0225@163.com>
2019-08-08 16:59:26 +0800

    Programs that Surf the Web

---
+++ Following Links in HTML Using BeautifulSoup.py
@@ -0,0 +1,36 @@
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter url - ')
+count = int(input('Enter count -'))
+position = int(input('Enter position -'))
+while count > 0:
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    tag = tags[position-1]
+    url = tag.get('href', None)
+    print('Retrieving:', url)
+    count = count - 1
+
+
+
+
+
+
